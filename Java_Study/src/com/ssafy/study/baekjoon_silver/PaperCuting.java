76531eafa45cddefdae33f6eef71bb9fd4309f4c
dongs <138917930+dongschiken@users.noreply.github.com>
2024-07-25 00:21:35 +0900

    백준_종이자르기 추가

--- Java_Study/src/com/ssafy/study/baekjoon_silver/PaperCuting.java
+++ Java_Study/src/com/ssafy/study/baekjoon_silver/PaperCuting.java
@@ -1,66 +1,51 @@
-//package com.ssafy.study.baekjoon_silver;
-//
-//import java.io.BufferedReader;
-//import java.io.BufferedWriter;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.io.OutputStreamWriter;
-//import java.util.Arrays;
-//import java.util.StringTokenizer;
-//
-//public class PaperCuting {
-//	static int[][] map;
-//	public static void main(String[] args) throws NumberFormatException, IOException {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//		StringTokenizer st;
-//		int N = Integer.parseInt(br.readLine());
-//		int M = Integer.parseInt(br.readLine());
-//		int T = Integer.parseInt(br.readLine());
-//		int[] dr = {1, -1, 0, 0};
-//		int[] dc = {0, 0, 1, -1};
-//		map = new int[M][N];
-//		
-//		Arrays.fill(map, 1);
-//		for (int i = 0; i < T; i++) {
-//			st = new StringTokenizer(br.readLine());
-//			int command = Integer.parseInt(st.nextToken());
-//			int startPoint = Integer.parseInt(st.nextToken());
-//
-//			// 만약 0이면 가로방향으로 N지점 까지 반복해서 1로 초기화 해준다.
-//			if(command == 0) {
-//				for (int j = 0; j < N; j++) {
-//					map[startPoint++][0] = 0;
-//				}
-//			} else {
-//				for (int j = 0; j < M; j++) {
-//					map[0][startPoint++] = 0;
-//				}
-//			}
-//			// nr, nc가 1이면 +1 / 0이면 +0
-//		}
-//		OUT:
-//		while (true) {
-//			int count = 0;
-//			
-//			for (int i = 0; i < M; i++) {
-//				for (int j = 0; j < N; j++) {
-//					if(map[i][j] == 1) {
-//						paperCheck(j, i, count);
-//						continue OUT;
-//					}
-//				}
-//			}
-//			break;
-//		}
-//		
-//	}
-//
-//	private static int paperCheck(int j, int i, int count) {
-//		
-//		
-//		
-//		return count;
-//	}
-//	
-//}
+package com.ssafy.study.baekjoon_silver;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class PaperCuting {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int C = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(br.readLine());
+		
+		int[] row = new int[R+1];
+		int[] col = new int[C+1];
+		
+		for (int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int command = Integer.parseInt(st.nextToken());
+			int index = Integer.parseInt(st.nextToken());
+			
+			if(command == 1) {
+				col[index] = 1;
+			}else {
+				row[index] = 1;
+			}
+		}
+		int max = Integer.MIN_VALUE;
+		bw.write(paperCheck(max, col, C)*paperCheck(max, row, R)+"");
+		bw.flush();
+	}
+
+	private static int paperCheck(int max, int[] rcArr, int rc) {
+		int count = 0;
+		for (int i = 1; i <= rc; i++) {
+			count++;
+			if(rcArr[i] == 1) {
+				max = Math.max(max, count);
+				count = 0;
+			}
+		}
+		max = Math.max(max, count);
+		return max;
+	}
+	
+}
