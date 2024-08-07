972796d15e2011c1c1fe3e4cb96daa3e0d5b1ede
dongschicken <138917930+dongschiken@users.noreply.github.com>
2024-08-07 23:58:51 +0900

    2024-08-07 / 패턴마디의길이_이동영

--- 5_24.08.13(화)/PatternTest_SWEA_2007_패턴마디의길이_이동영.java
+++ 5_24.08.13(화)/PatternTest_SWEA_2007_패턴마디의길이_이동영.java
@@ -14,10 +14,15 @@ public class PatternTest_SWEA_2007_패턴마디의길이_이동영 {
 		for (int i = 1; i <= T; i++) {
 			String str = br.readLine();
 			int index = 0;
+			// 어차피 마디의 최대 길이가 10개까지니까
 			for (int j = 1; j <= 10; j++) {
-				
+				// 0, 1 == 1, 2 비교 1개의 문자열 비교
+				// 0, 2 == 2, 4 비교 2개의 문자열 비교
+				// ...
+				// 0, 5 == 5, 10 비교 5개의 문자열 비교 
 				String tmp1 = str.substring(0, j);
 				String tmp2 = str.substring(j, j*2);
+				// 같은 문자열이 나왔을 경우의 길이를 index에 넣고 출력
 				if(tmp2.equals(tmp1)) {
 					index = j;
 					break;
