7421c6043fd9870ef3576a428cc5693487ea6afb
judygrkim <111031630+judygrkim@users.noreply.github.com>
2024-08-07 19:44:21 +0900

    Update SWEA_7272_안경이없어_김규리.java

--- 5_24.08.13(화)/SWEA_7272_안경이없어_김규리.java
+++ 5_24.08.13(화)/SWEA_7272_안경이없어_김규리.java
@@ -13,14 +13,17 @@ public class Solution7272_안경이없어 {
 
         String ans = "SAME";
 
+        //스트링 두 묶음 만들기 
         String AD = "ADOPQR";
         String CE = "CEFGHIJKLMNSTUVWXYZ";
 
+        //앞에서부터 하나씩 뜯어서 비교 
         if (first.length() == second.length()) {
             for (int d = 0; d < first.length(); d++) {
                 String f = Character.toString(first.charAt(d));
                 String s = Character.toString(second.charAt(d));
 
+                //둘 다 AD의 알파벳과 겹치거나 CE 알파벳과 겹치거나 하면 넘어가고 그 외는 DIFF로 바뀜 
                 if ((f.equals("B") && s.equals("B"))
                     || (AD.contains(f) && AD.contains(s))
                     || (CE.contains(f) && CE.contains(s))) {
@@ -30,6 +33,7 @@ public class Solution7272_안경이없어 {
                 }
 
             }
+            //길이가 다르면 DIFF
         } else {
             ans = "DIFF";
         }
