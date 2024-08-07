654bed0fed5467571db33b3a32eddd6f4980c816
dongschicken <138917930+dongschiken@users.noreply.github.com>
2024-08-07 23:55:01 +0900

    2024-08-07 퍼펙트셔플_이동영

--- 5_24.08.13(화)/PerfectShuffle_SWEA_3499_퍼펙트셔플_이동영.java
+++ 5_24.08.13(화)/PerfectShuffle_SWEA_3499_퍼펙트셔플_이동영.java
@@ -13,9 +13,11 @@ public class PerfectShuffle_SWEA_3499_퍼펙트셔플_이동영 {
 		
 		for (int i = 0; i < T; i++) {
 			int N = Integer.parseInt(br.readLine());
+			//  5가나오면 3으로 만들고, 7이나오면 4로 만든다.
 			int half = N - N/2;
 			String[] str = br.readLine().split(" ");
-			
+
+			// 5가 나왔을 경우 K는 0->1->2 순으로 증가 / half는 3-4 순으로 증가
 			for (int j = 0, k = 0; j < str.length; j++) {
 				if(j % 2 == 0) {
 					sb.append(str[k++]).append(" ");
